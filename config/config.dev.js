907676f7bd417e2177c3c56bedd7aa514ef0be0c
wqjiao <wqjiao@che300.com>
2020-07-31 15:39:28 +0800

    config:增加打包hash

--- config/config.dev.js
+++ config/config.dev.js
@@ -1,7 +1,12 @@
+import {hash} from './constants';
+
 export default {
-    publicPath: 'https://raw.githubusercontent.com/wqjiao/bpmn-activiti/master/public',
+    outputPath: `./dist/${hash}`, // 打包路径增加hash值层，方便删除
+    publicPath: `https://raw.githubusercontent.com/wqjiao/bpmn-activiti/master/public/${hash}/`,
     define: {
         'process.env.apiUrl': 'https://raw.githubusercontent.com/wqjiao/bpmn-activiti',
+        'process.env.assetsUrl':
+            'https://raw.githubusercontent.com/wqjiao/bpmn-activiti/master/public', // 资源地址
     },
     hash: true,
 };
